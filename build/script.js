de930b1461c54622aaa00d5db3f1013eaa2ef5d9
Carmen <carmendinu5@yahoo.com>
2020-10-14 09:15:26 +0300

    [master] update

--- build/script.js
+++ build/script.js
@@ -24,36 +24,36 @@ function takeaLetter() {
 	let isLetterCorrect = guessedArray.includes(input);
 	if (!isLetterCorrect) {
 		livesLeft--;
-		document.getElementById('lives').innerHTML = `You have ${livesLeft} left lives`;
+		document.getElementById('lives').innerHTML = `You have ${livesLeft} remaining lives`;
 	}
 	if (livesLeft === 3) {
 		let firstDiv = document.getElementById('firstDiv');
 		firstDiv.classList.add('firstDiv');
-	} else if (livesLeft === 2) {
 		let secondDiv = document.getElementById('secondDiv');
 		secondDiv.classList.add('secondDiv');
-	} else if (!isLetterCorrect && livesLeft === 1) {
 		let thirdDiv = document.getElementById('thirdDiv');
 		thirdDiv.classList.add('thirdDiv');
-	} else if (livesLeft === 0) {
+	} else if (livesLeft === 2) {
 		let forthdDiv = document.getElementById('forthdDiv');
 		forthdDiv.classList.add('forthdDiv');
 		let fifthdDiv = document.getElementById('fifthdDiv');
 		fifthdDiv.classList.add('fifthdDiv');
 		let sixthDiv = document.getElementById('sixthDiv');
 		sixthDiv.classList.add('sixthDiv');
+	} else if (!isLetterCorrect && livesLeft === 1) {
 		let seventhDiv = document.getElementById('seventhDiv');
 		seventhDiv.classList.add('seventhDiv');
 		let eigthDiv = document.getElementById('eigthDiv');
 		eigthDiv.classList.add('eigthDiv');
+	} else if (livesLeft === 0) {
 		let ninethDiv = document.getElementById('ninethDiv');
 		ninethDiv.classList.add('ninethDiv');
 		let tenthDiv = document.getElementById('tenthDiv');
 		tenthDiv.classList.add('tenthDiv');
-		document.getElementById('lives').innerHTML = `Looser`;
+		document.getElementById('lives').innerHTML = `Try another one`;
 	} else if (livesLeft <= 0) {
 		livesLeft--;
-		document.getElementById('lives').innerHTML = `Looser`;
+		document.getElementById('lives').innerHTML = `Really now! Press the Try another one button`;
 		return;
 	}
 
